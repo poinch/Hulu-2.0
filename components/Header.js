a64e29f91b674291a3121e6e1f63e577aef3ba67
Poinch <ponch92@hotmail.it>
2021-09-12 14:39:19 +0200

    Built Header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,30 @@
+import Image from 'next/image'
+import logo from '../public/hulu-white.png'
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline'
+import HeaderItem from './HeaderItem';
+
 function Header() {
   return (
-    <header>
-      <h1>This is the header</h1>
+    <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
+      <div className='flex flex-grow justify-evenly mt-3 max-w-2xl'>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
+      </div>
+      <Image 
+        className='object-contain'
+        src={logo} width={200} height={100} 
+      />
     </header>
   );
 }
