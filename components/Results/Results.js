6ca2f4ad8ec02f237280b5874c6ba539fb99c87d
Poinch <ponch92@hotmail.it>
2021-09-15 11:05:03 +0200

    remove unused props

--- components/Results/Results.js
+++ components/Results/Results.js
@@ -1,10 +1,10 @@
 import Thumbnail from './Thumbnail'
 
-function Results({ results, clicked }) {
+function Results({ results }) {
   return (
     <div className='px-5 py-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
       {results.map((result) => (
-        <Thumbnail clicked key={result.id} result={result} />
+        <Thumbnail key={result.id} result={result} />
       ))}
     </div>
   )
